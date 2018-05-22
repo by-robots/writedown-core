e4be2c6626a71e31586bd4d65b911865f456f936
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2018-05-22 13:28:41 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Providers/ValidationServiceProvider.php
+++ src/Providers/ValidationServiceProvider.php
@@ -10,28 +10,28 @@ use League\Container\ServiceProvider\AbstractServiceProvider;
 
 class ValidationServiceProvider extends AbstractServiceProvider
 {
-	/**
-	 * Services provided by the service provider.
-	 *
-	 * @var array
-	 */
-	protected $provides = [
-		'ByRobots\WriteDown\Validator\ValidatorInterface',
-	];
+    /**
+     * Services provided by the service provider.
+     *
+     * @var array
+     */
+    protected $provides = [
+        'ByRobots\WriteDown\Validator\ValidatorInterface',
+    ];
 
-	/**
-	 * Register providers into the container.
-	 */
-	public function register()
-	{
-		$this->getContainer()
-		     ->add('ByRobots\WriteDown\Validator\ValidatorInterface', ByRobots::class);
+    /**
+     * Register providers into the container.
+     */
+    public function register()
+    {
+        $this->getContainer()
+                ->add('ByRobots\WriteDown\Validator\ValidatorInterface', ByRobots::class);
 
-		$this->getContainer()
-             ->inflector('ByRobots\WriteDown\Validator\ValidatorInterface')
-             ->invokeMethod('addRules', [[
-                 new Unique($this->getContainer()->get('Doctrine\ORM\EntityManagerInterface')),
-                 new ValidSlug(new GenerateSlug),
+        $this->getContainer()
+                ->inflector('ByRobots\WriteDown\Validator\ValidatorInterface')
+                ->invokeMethod('addRules', [[
+                    new Unique($this->getContainer()->get('Doctrine\ORM\EntityManagerInterface')),
+                    new ValidSlug(new GenerateSlug),
             ]]);
-	}
+    }
 }
