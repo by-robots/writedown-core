c757f1e58dbdfe900e9a0894de0e51e04dfbfa19
By Robots <skynet@by-robots.com>
2018-10-01 16:33:30 +0100

    Correct mistake in migration.

--- db/migrations/20181001152707_add_detached_column_to_posts.php
+++ db/migrations/20181001152707_add_detached_column_to_posts.php
@@ -1,14 +1,12 @@
 <?php
 
-
 use Phinx\Migration\AbstractMigration;
 
 class AddDetachedColumnToPosts extends AbstractMigration
 {
     public function change()
     {
-        $table = $this->table('posts');
-        $table->add_column('detached', 'boolean', [
+        $table = $this->table('posts')->addColumn('detached', 'boolean', [
             'default' => false,
         ]);
     }
