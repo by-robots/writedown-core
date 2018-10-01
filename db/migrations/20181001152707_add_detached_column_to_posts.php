972dec6952fdc8336449449ad42ef4262749ed8c
By Robots <skynet@by-robots.com>
2018-10-01 16:40:26 +0100

    Actually fix the migration this time, and update the test DB.

--- db/migrations/20181001152707_add_detached_column_to_posts.php
+++ db/migrations/20181001152707_add_detached_column_to_posts.php
@@ -6,8 +6,9 @@ class AddDetachedColumnToPosts extends AbstractMigration
 {
     public function change()
     {
-        $table = $this->table('posts')->addColumn('detached', 'boolean', [
+        $table = $this->table('posts');
+        $table->addColumn('detached', 'boolean', [
             'default' => false,
-        ]);
+        ])->update();
     }
 }
